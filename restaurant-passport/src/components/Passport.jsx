98832ee7df0cb2370d0399776ac4c49641c52b29
Braden <fish2355@hotmail.com>
2020-01-08 12:04:29 -0700

    added search functionality for user passport

--- restaurant-passport/src/components/Passport.jsx
+++ restaurant-passport/src/components/Passport.jsx
@@ -2,53 +2,49 @@ import React, { useState, useEffect } from "react";
 import Restaurant from "./Restaurant";
 import axiosWithAuth from "../utils/index";
 
-
 function Passport({ setFlipped, flipped }) {
-
   const [passportList, setList] = useState([]);
   const user_id = localStorage.getItem("user_id");
   const [input, setInput] = useState({
     search: ""
   });
   const [search, setSearch] = useState(true);
+
   useEffect(() => {
     axiosWithAuth()
       .get(`https://rpass.herokuapp.com/api/users/${user_id}/passport`)
       .then(res => {
-        // console.log("passport", res.data);
+        console.log("passport", res.data);
         setList(res.data);
       })
       .catch(err => console.log(err));
-  }, [user_id, flipped]);
+  }, [user_id, flipped, search]);
 
   const deleteRestaurant = id => {
-    console.log(id)
+    console.log(id);
     axiosWithAuth()
       .delete(`/users/${user_id}/passport/${id}`)
       .then(res => {
         console.log(res);
         console.log(id);
-        this.props.history.push('/passport');
+        this.props.history.push("/passport");
       })
       .catch(err => {
         console.log(err.message);
-      })
-  }
-
-  // useEffect(() => {
-  //   axiosWithAuth()
-  //     //  todo get searching endpoint here
-  //     .get(`search endpoint`)
-  //     .then(res => {
-  //       console.log("passportSearch", res.data);
-  //       setList(res.data);
-  //     })
-  //     .catch(err => console.log(err));
-  // }, [search]);
+      });
+  };
 
   const onFormSubmit = evt => {
     evt.preventDefault();
-    setSearch(!search);
+    console.log("passportList", passportList, "search", input.search);
+    input.search === ""
+      ? setSearch(!search)
+      : setList(
+          passportList.filter(e =>
+            e.name.toLowerCase().includes(input.search.toLowerCase())
+          )
+        );
+
     setInput({
       search: ""
     });
@@ -76,7 +72,12 @@ function Passport({ setFlipped, flipped }) {
       </form>
       <div className="passport-list">
         {passportList.map(e => (
-          <Restaurant restaurant={e} setFlipped={setFlipped} deleteRestaurant={deleteRestaurant} flipped={flipped} />
+          <Restaurant
+            restaurant={e}
+            setFlipped={setFlipped}
+            deleteRestaurant={deleteRestaurant}
+            flipped={flipped}
+          />
         ))}
       </div>
     </>
